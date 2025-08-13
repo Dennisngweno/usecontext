0eebc9c721f03bcf19e315ede137f47d3a1c5370
Dennisngweno <ngwenodennis@gmail.com>
2025-08-13 09:45:34 +0300

    my first commit

---
+++ src/Components/Level3.jsx
@@ -0,0 +1,13 @@
+function Level3() {
+    const userAuth = {
+        Uname: Dennis,
+        Id: 30757894,
+        isLoggedIn: true
+    }
+    return(
+        <>
+        
+        </>
+    )
+}
+export default  Level3
